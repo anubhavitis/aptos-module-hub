b758eb0b2c754665d9d86ed365e16664698ad722
Anubhav Singhal <ss.lfsgd@gmail.com>
2024-09-15 18:47:19 +0530

    bugfix

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,8 +7,8 @@ mod shell;
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
-            shell::publish_to_devnet,
             shell::account_modules,
+            shell::publish_to_devnet
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
